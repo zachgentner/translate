3814118b62f63104394afe735e8f784b7c50d2b7
Zach Gentner <zach.gentner@gmail.com>
2023-11-08 20:06:34 -0500

    changes

--- script.js
+++ script.js
@@ -4,54 +4,74 @@ let dict = {
     test: "prueba",
 }
 
-word.addEventListener('click', () => {
-    translateWord(word)
+word.addEventListener('click', async () => {
+    await fadeOut(word);
+    translateWord(word);
+    await fadeIn(word);
 });
 
-function fadeOut(element) {
+async function fadeOut(element) {
     var opacity = 1;  // Initial opacity
+    const fadeDuration = 500; // Duration in milliseconds
+    const opacityInterval = 0.1;
 
-    // Create an interval to gradually reduce the opacity
-    var fadeInterval = setInterval(function () {
-        if (opacity <= 0) {
-            // When opacity is 0 or negative, stop the interval
-            clearInterval(fadeInterval);
-            // element.style.display = "none"; // Optionally hide the element
-            //Load translated word.
-        } else {
-            opacity -= 0.1; // Decrease opacity in steps
-            element.style.opacity = opacity;
-        }
-    }, 50); // Adjust the interval time (in milliseconds) as needed
+    return new Promise(async (resolve) => {
+        const fadeOutAnimation = () => {
+            opacity -= opacityInterval;
+            if (opacity <= 0) {
+                element.style.opacity = 0;
+                resolve();
+            } else {
+                element.style.opacity = opacity;
+                setTimeout(fadeOutAnimation, fadeDuration * opacityInterval);
+            }
+        };
+
+        fadeOutAnimation();
+    });
 }
 
-function fadeIn(element) {
+async function fadeIn(element) {
     var opacity = 0; // Initial opacity
+    const fadeDuration = 500; // Duration in milliseconds
+    const opacityInterval = 0.1;
 
     element.style.display = "block"; // Ensure the element is visible
 
-    // Create an interval to gradually increase the opacity
-    var fadeInterval = setInterval(function () {
-        if (opacity >= 1) {
-            // When opacity is 1 or greater, stop the interval
-            clearInterval(fadeInterval);
-        } else {
-            opacity += 0.1; // Increase opacity in steps
-            element.style.opacity = opacity;
-        }
-    }, 100); // Adjust the interval time (in milliseconds) as needed
+    return new Promise(async (resolve) => {
+        const fadeInAnimation = () => {
+            opacity += opacityInterval;
+            if (opacity >= 1) {
+                element.style.opacity = 1;
+                resolve();
+            } else {
+                element.style.opacity = opacity;
+                setTimeout(fadeInAnimation, fadeDuration * opacityInterval);
+            }
+        };
+
+        fadeInAnimation();
+    });
 }
 
-async function translateWord(element) {
-    await fadeOut(element);
+function translateWord(element) {
     const word = element.innerText;
     let translation = dict[element.innerText.toLowerCase()];
 
-    // If the first letter of the word being translated is capitalized, capitalize the first letter of the translation.
+    if (dict[element.innerText.toLowerCase()]) {
+        translation = dict[element.innerText.toLowerCase()];
+    } else {
+        for (let value in dict) {
+            if (dict[value].toLowerCase() === word.toLowerCase()) {
+                translation = value;
+            }
+        }
+    }
+
+    //Capitalizes the word if the translated word was also capitalized.
     if (/[A-Z]/.test(word[0])) {
         translation = translation[0].toUpperCase() + translation.slice(1, translation.length);
     }
 
     element.innerHTML = translation;
-    // fadeIn(element);
 }
